8c03604c8c7eda94b96b8d4cbcfbec11dafe8006
gibus <gibus@easter-eggs.com>
2018-12-06 23:17:44 +0100

    Fix pagination for CustomFieldValues collection

--- lib/RT/Extension/REST2/Resource/CustomFieldValues.pm
+++ lib/RT/Extension/REST2/Resource/CustomFieldValues.pm
@@ -8,6 +8,8 @@ use namespace::autoclean;
 extends 'RT::Extension::REST2::Resource::Collection';
 with 'RT::Extension::REST2::Resource::Collection::QueryByJSON';
 
+use POSIX qw(ceil);
+
 has 'customfield' => (
     is  => 'ro',
     isa => 'RT::CustomField',
@@ -43,6 +45,21 @@ sub serialize {
     my $cf = $self->customfield;
     my @results;
 
+    my $per_page = $self->request->param('per_page') || 20;
+    if    ($per_page !~ /^\d+$/ ) { $per_page = 20  }
+    elsif ($per_page == 0       ) { $per_page = 20  }
+    elsif ($per_page > 100      ) { $per_page = 100 }
+    $collection->RowsPerPage($per_page);
+
+    my $count_all = $collection->CountAll;
+    my $max_page = ceil($count_all / $per_page);
+
+    my $page = $self->request->param('page') || 1;
+    if    ($page !~ /^\d+$/  ) { $page = 1 }
+    elsif ($page == 0        ) { $page = 1 }
+    elsif ($page > $max_page ) { $page = $max_page }
+    $self->collection->GotoPage($page - 1);
+
     while (my $item = $collection->Next) {
         my $result = {
             type => 'customfieldvalue',
@@ -53,17 +70,36 @@ sub serialize {
         push @results, $result;
     }
 
-    # DBIx::SearchBuilder::CountAll() seems buggy
-    # returning outdated cached value, force recount
-    $collection->{count_all} = 0;
-
-    return {
-        count       => scalar(@results)         + 0,
-        total       => $collection->CountAll    + 0,
-        per_page    => $collection->RowsPerPage + 0,
-        page        => ($collection->FirstRow / $collection->RowsPerPage) + 1,
+    my %results = (
+        count       => scalar(@results) + 0,
+        total       => $count_all       + 0,
+        per_page    => $per_page        + 0,
+        page        => $page            + 0,
         items       => \@results,
+    );
+
+    my $uri = $self->request->uri;
+    my @query_form = $uri->query_form;
+    # find page and if it is set, delete it and it's value.
+    for my $i (0..$#query_form) {
+        if ($query_form[$i] eq 'page') {
+            delete @query_form[$i, $i + 1];
+            last;
+        }
+    }
+
+    $results{pages} = ceil($results{total} / $results{per_page});
+    if ($results{page} < $results{pages}) {
+        my $page = $results{page} + 1;
+        $uri->query_form( @query_form, page => $results{page} + 1 );
+        $results{next_page} = $uri->as_string;
     };
+    if ($results{page} > 1) {
+        $uri->query_form( @query_form, page => $results{page} - 1 );
+        $results{prev_page} = $uri->as_string;
+    };
+
+    return \%results;
 }
 
 __PACKAGE__->meta->make_immutable;
